2d7abeda0b3438707fa55714754a3cbaef82e85e
Sjir Bagmeijer <sbagmeijer@ulyaoth.asia>
2013-09-09 19:36:01 +0200

    Update createadminaccount.pl

--- createadminaccount.pl
+++ createadminaccount.pl
@@ -13,7 +13,7 @@ use Digest::SHA2;
 use Data::UUID;
 
 
-my $yaml = YAML::Tiny->read( '/ulyaothchat/configuration/config.yml' );
+my $yaml = YAML::Tiny->read( 'config.yml' );
 
 my $db_driver = $yaml->[0]->{database}->{driver};
 my $db_host = $yaml->[0]->{database}->{host};
@@ -25,27 +25,36 @@ my $db_pass = $yaml->[0]->{database}->{password};
 my $dbh = DBI->connect("DBI:$db_driver:host=$db_host:$db_port;database=$db_name",$db_user,$db_pass)
     or die "Unable to connect to database: \"$DBI::errstr\" $! \n";
     
-my $member = "INSERT INTO members(first_name, last_name, email_address, country, city, birthday) VALUES('James', 'bond', 'james\@bond.com', 'England', 'London', '1946-12-11')";
-my $sth = $dbh->prepare($member) or die "Cannot prepare: " . $dbh->errstr();
+my $formdata = "jamesbond\@live.com";
+
+my $sth = $dbh->prepare( 'SELECT email_address FROM members WHERE email_address = "jamesbond@live.com" ') or die "Couldn't prepare statement: " . $dbh->errstr;
+my @row = $dbh->selectrow_array( $sth,{} );
+if( @row ) {
+print "Sorry your this email address is already in use.";
+$sth->finish();
+}else{
+my $member = "INSERT INTO members(first_name, last_name, email_address, country, city, birthday) VALUES('James', 'Bond', 'jamesbond\@live.com', 'United Kingdom', 'London', '1963-9-14')";
+$sth = $dbh->prepare($member) or die "Cannot prepare: " . $dbh->errstr();
 $sth->execute() or die "Cannot execute: " . $sth->errstr();
 $sth->finish();
 
-my $uniqueid = qq`SELECT uniqueid FROM members WHERE email_address="james\@bond.com"`;
-$sth = $dbh->prepare($uniqueid) or die "Cannot prepare: " . $dbh->errstr();
+$sth = $dbh->prepare( 'SELECT user_id FROM members WHERE email_address = "jamesbond@live.com" ') or die "Couldn't prepare statement: " . $dbh->errstr;
 $sth->execute() or die "Cannot execute: " . $sth->errstr();
-$uniqueid = $sth->fetchrow_array();
+my $user_id = $sth->fetchrow_array();
 $sth->finish();
 
 my $ug = new Data::UUID;
 $ug->create_str();
 
-my $chatid = $ug->create_from_name_str(NameSpace_URL, "$uniqueid");
-my $password = "JamesBondIsSoStrong123456";
+my $member_id = $ug->create_from_name_str(NameSpace_URL, "$user_id");
+my $unhashedpassword = "JamesBondIsCool007";
 my $sha2obj = new Digest::SHA2 512;
-$sha2obj->add($password, $chatid);
-my $digest = $sha2obj->hexdigest();
+$sha2obj->add($unhashedpassword, $member_id);
+my $hashedpassword = $sha2obj->hexdigest();
 
-my $memberdata = "INSERT INTO member_data(chatid, username, password, exempt) VALUES('$chatid', 'Admin', '$digest', 'Administrator')";
+my $memberdata = "INSERT INTO member_data(user_id, member_id, username, password, exempt) VALUES('$user_id', '$member_id', 'Admin, '$hashedpassword', 'Administrator')";
 $sth = $dbh->prepare($memberdata) or die "Cannot prepare: " . $dbh->errstr();
 $sth->execute() or die "Cannot execute: " . $sth->errstr();
 $sth->finish();
+
+}
